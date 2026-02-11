39bb37354f693a290369bc1e59bae652493cac0f
premsaikamadani <saikamadani148@gmail.com>
2026-02-11 13:22:12 +0530

    get api created

--- grocery/groceryapp/urls.py
+++ grocery/groceryapp/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 
 urlpatterns = [
-    path('create/', views.grocery_create,name='create_grocery')
+    path('create/', views.grocery_create,name='create_grocery'),
+    path('', views.grocery_list, name='grocery_list'),
 ]
--- grocery/groceryapp/views.py
+++ grocery/groceryapp/views.py
@@ -1,14 +1,20 @@
 from django.shortcuts import render
+from .models import Grocery
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
 from .serializers import GrocerySerializer
 
-# Create your views here.
 @api_view(['POST'])
 def grocery_create(request):
-    serializer = GrocerySerializer(data = request.data)
+    serializer = GrocerySerializer(data=request.data)
     if serializer.is_valid():
         serializer.save()
-        return Response(serializer.data, status = 201)
-    else:
-        return Response(serializer.errors,status = 400)
+        return Response(serializer.data, status=201)
+    return Response(serializer.errors, status=400)
+
+
+@api_view(['GET'])
+def grocery_list(request):
+    groceries = Grocery.objects.all()
+    serializer = GrocerySerializer(groceries, many=True)
+    return Response(serializer.data,status=200)
